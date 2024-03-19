e7a65c4460b4ed3e6d4889fc2d7811d6ab428fbd
rafaseron <rafaelseron3011@gmail.com>
2024-03-19 06:31:10 -0400

    gerar Id automatico por UUID

--- app/src/main/java/br/com/alura/panucci/model/Product.kt
+++ app/src/main/java/br/com/alura/panucci/model/Product.kt
@@ -1,8 +1,10 @@
 package br.com.alura.panucci.model
 
 import java.math.BigDecimal
+import java.util.UUID
 
 class Product(
+    val iD: String = UUID.randomUUID().toString(),
     val name: String,
     val price: BigDecimal,
     val description: String,
