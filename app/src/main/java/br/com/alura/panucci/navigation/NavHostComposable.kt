a7cd15595c93adb43b11eca0e50faa33fc9d16ed
rafaseron <rafaelseron3011@gmail.com>
2024-03-20 17:22:51 -0400

    Como Aninhar Graphos de Navegacao

--- app/src/main/java/br/com/alura/panucci/navigation/NavHostComposable.kt
+++ app/src/main/java/br/com/alura/panucci/navigation/NavHostComposable.kt
@@ -1,17 +1,35 @@
 package br.com.alura.panucci.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.navigation
 
 @Composable
 fun NavHostComposable(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = highlightListRoute) {
+    NavHost(navController = navController, startDestination = homeGraph) {
+        //no startDestination ele acessa o homeGraph e é o startDestination do homeGraph quem determina qual é o startDestination
+        /*
+        Assim, direto, é como era, antes de Aninhar os Graphos de Navegação
         highlightListScreen(navController)
-        checkoutScreen(navController)
         menuListScreen(navController)
         drinksListScreen(navController)
-        productDetailsScreen(navController)
-    }
+         */
+        homeGraph(navController)    //essa tecnica de Aninhar Graphos é complexa, mas tu tem que saber que ela existe - porque vai tu trabalhe em um app que seja assim
 
+        productDetailsScreen(navController)
+        checkoutScreen(navController)
+    }
+}
+
+// NO CASO, ESTES DADOS ABAIXO PODEM ESTAR EM UM OUTRO ARQUIVO, POR EXEMPLO HomeGraphNavigation.kt
+internal const val homeGraph = "homeGraphRoute"
+fun NavGraphBuilder.homeGraph(navController: NavHostController){
+    navigation(route = homeGraph, startDestination = highlightListRoute){
+        highlightListScreen(navController)
+        menuListScreen(navController)
+        drinksListScreen(navController)
+    }
 }
