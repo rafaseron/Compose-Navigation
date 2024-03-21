e47d5dde640fbaebfa550bb6410575aa5961ffb0
rafaseron <rafaelseron3011@gmail.com>
2024-03-21 13:48:54 -0400

    add ProductDao

---
+++ app/src/main/java/br/com/alura/panucci/dao/ProductDao.kt
@@ -0,0 +1,33 @@
+package br.com.alura.panucci.dao
+
+import br.com.alura.panucci.model.Product
+import br.com.alura.panucci.sampledata.sampleProducts
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import java.math.BigDecimal
+
+class ProductDao{
+
+    private val _products = MutableStateFlow(sampleProducts)
+    val products = _products.asStateFlow()
+
+    fun findByIdWithResponse(id: String): Product{
+        val listProducts = products.value
+        val searchProduct = listProducts.find {
+                p ->
+                p.iD == id
+        }
+        searchProduct?.let {
+            product ->
+            return product
+        } ?: return Product("", "", BigDecimal.ZERO, "", null)
+    }
+    fun findById(id: String){
+        val listProducts = products.value
+        listProducts.find {
+            p ->
+            p.iD == id
+        }
+    }
+
+}
